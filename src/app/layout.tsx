a1e7ac394a4eb9453ba63cfbfee6185f20bff803
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-04-28 17:54:36 +0900

    認証前のヘッダーを作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,19 +15,36 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <link
-          href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link rel="stylesheet" href="//demo.productionready.io/main.css" />
       </Head>
-      <body>{children}</body>
+      <body>
+        <header>
+          <nav className="navbar navbar-light">
+            <div className="container">
+              <a className="navbar-brand" href="/">
+                conduit
+              </a>
+              <ul className="nav navbar-nav pull-xs-right">
+                <li className="nav-item">
+                  <a className="nav-link active" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/login">
+                    Sign in
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/register">
+                    Sign up
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
