706e751d940b8c846c1a26370057f6d9117aa76a
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-04-28 18:39:01 +0900

    Bannerコンポーネントに分離

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
+import { Banner } from "./components/Banner";
+
 export default function Home() {
   return (
     <div className="home-page">
-      <div className="banner">
-        <div className="container">
-          <h1 className="logo-font">conduit</h1>
-          <p>A place to share your knowledge.</p>
-        </div>
-      </div>
+      <Banner />
 
       <div className="container page">
         <div className="row">
