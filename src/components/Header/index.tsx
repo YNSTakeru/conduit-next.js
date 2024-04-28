18445a94766e8cb58ae2279eece68392c4dedf5d
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-04-28 23:25:18 +0900

    Headerのリンクを修正

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,22 +4,22 @@ export default function Header() {
   return (
     <nav className="navbar navbar-light">
       <div className="container">
-        <a className="navbar-brand" href="index.html">
+        <a className="navbar-brand" href="/">
           conduit
         </a>
         <ul className="nav navbar-nav pull-xs-right">
           <li className="nav-item">
-            <a className="nav-link active" href="">
+            <a className="nav-link active" href="/">
               Home
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link" href="/login">
               Sign in
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link" href="/register">
               Sign up
             </a>
           </li>
