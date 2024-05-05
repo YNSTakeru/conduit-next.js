d08af45b55208218fd2cc0406b097fac98228d79
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-06 01:33:58 +0900

    ログイン機能の実装

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,14 +2,10 @@ import axios from "axios";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const { username, email, password } = await req.json();
+  const { email, password } = await req.json();
 
-  if (!username || !email || !password) {
-    let response;
+  if (!email || !password) {
     const errorMessages = [];
-    if (!username) {
-      errorMessages.push("please type username");
-    }
     if (!email) {
       errorMessages.push("please type email");
     }
@@ -17,16 +13,15 @@ export async function POST(req: NextRequest) {
       errorMessages.push("please type password");
     }
 
-    response = new Response(errorMessages.join(", "), { status: 400 });
-    return response;
+    return new Response(errorMessages.join(", "), { status: 400 });
   }
 
   const data = {
-    user: { username, email, password },
+    user: { email, password },
   };
 
   try {
-    const response = await axios.post("http://localhost/api/users", data);
+    const response = await axios.post("http://localhost/api/users/login", data);
 
     const token = response.data.user.token;
     const cookie = `token=${token}; Path=/; HttpOnly; SameSite=Lax; Max-Age=3600;`;
@@ -45,9 +40,12 @@ export async function POST(req: NextRequest) {
   } catch (error) {
     if (axios.isAxiosError(error)) {
       if (error.response) {
-        const errors = error.response.data.errors;
-        const errorsString = JSON.stringify(errors);
-        return new Response(errorsString, { status: 500 });
+        if (error.response.data.errors) {
+          const errors = error.response.data.errors;
+          const errorsString = JSON.stringify(errors);
+          return new Response(errorsString, { status: 500 });
+        }
+        return new Response(error.response.data.message, { status: 500 });
       }
     }
   }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,48 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessages, setErrorMessages] = useState<string[]>([]);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.status === 400) {
+      const data = await response.text();
+
+      const dataList = data.split(", ");
+      setErrorMessages(() => dataList);
+    }
+
+    if (response.status === 500) {
+      const data = await response.text();
+
+      if (data === "Login information invalid") {
+        setErrorMessages(() => ["Login information invalid"]);
+        return;
+      }
+      const obj = JSON.parse(data);
+
+      const dataList = Object.keys(obj).map((key) => obj[key]);
+      setErrorMessages(() => dataList);
+    }
+
+    if (response.ok) {
+      window.location.href = "/";
+    }
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -12,22 +54,28 @@ export default function Login() {
             </p>
 
             <ul className="error-messages">
-              <li>That email is already taken</li>
+              {errorMessages.map((message, index) => (
+                <li key={index}>{message}</li>
+              ))}
             </ul>
 
-            <form>
+            <form onSubmit={handleSubmit}>
               <fieldset className="form-group">
                 <input
                   className="form-control form-control-lg"
-                  type="text"
+                  type="email"
+                  value={email}
                   placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </fieldset>
               <fieldset className="form-group">
                 <input
                   className="form-control form-control-lg"
                   type="password"
+                  value={password}
                   placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </fieldset>
               <button className="btn btn-lg btn-primary pull-xs-right">
